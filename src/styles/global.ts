cd50cf706c79f6662e839d5e77669dbaed3b9bcf
jadergermano <jader.germano@icloud.com>
2020-06-30 15:38:34 -0300

    default indent props added to tslint and global styles implemented

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import background from '../assets/background.svg';
 
 export default createGlobalStyle`
   *{
@@ -7,7 +8,25 @@ export default createGlobalStyle`
   outline: 0;
   box-sizing: border-box;
   }
-  body {
+ body {
+  // forreference about background in styled component literals
+  /*background: #f0f0f5 url(${background}) no-repeat 70% top;*/
   background: #f0f0f5;
+  -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+
+  }
+
+  #root {
+ /* max-width: 940px;
+  margin: 0 auto;
+  padding: 40px 20px;*/
+  }
+
+  button {
+  cursor: pointer;
   }
 `;
